1a1e14e0c8e048d9b5eaf769f7b47321ebf3f7dd
BoriszaZsigmondBenedek <borisza.zsigmond.benedek@students.jedlik.eu>
2024-03-08 09:40:32 +0100

    not work

--- menu.py
+++ menu.py
@@ -1,11 +1,16 @@
-class Menu: 
+import pygame
+
+
+class Menu:
+    restart_button = pygame.image.load('graphics/buttons/restart_button.png')
     def __init__(self, screen):
         self.screen = screen
 
     
     def pausemenu(self):
         self.screen.fill((0, 255, 0))
-    
+        self.screen.blit(self.restart_button(200, 200))
+
     def deathmenu(self):
         self.screen.fill((255, 0, 0,))
     
